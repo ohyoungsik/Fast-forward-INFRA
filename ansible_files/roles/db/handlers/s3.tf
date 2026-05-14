f4aa9038b6e2064a01d26bca815ef794ccecf742
choyongbin <jyb13579@naver.com>
2026-05-14 17:06:09 +0900

    2차(s3 버킷 생성 테라폼 iam 삭제)

--- ansible_files/roles/db/handlers/s3.tf
+++ ansible_files/roles/db/handlers/s3.tf
@@ -1,7 +1,7 @@
 # 1. AWS 서울 리전 및 키 설정 (하드코딩 방식)
 provider "aws" {
   region     = "ap-northeast-2"
-  access_key = "awskey"
+  access_key = "aws"
   secret_key = "aws"
 }
 
@@ -19,35 +19,6 @@ resource "aws_s3_bucket" "db_backup_bucket" {
     Name        = "Monitoring DB Backup"
     Environment = "Dev"
   }
-}
-
-# 4. fast-mgmt 서버가 찰 '보안 신분증(IAM Role)' 목걸이 생성
-resource "aws_iam_role" "ec2_s3_backup_role" {
-  name = "ktcloud-ec2-s3-backup-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-# 5. 그 신분증에 'S3 자유이용권(FullAccess)' 권한 스티커 붙이기
-resource "aws_iam_role_policy_attachment" "s3_full_access" {
-  role       = aws_iam_role.ec2_s3_backup_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-}
-
-# 6. EC2에 목걸이를 걸어주기 위한 연결고리(Instance Profile) 만들기
-resource "aws_iam_instance_profile" "ec2_s3_backup_profile" {
-  name = "ktcloud-ec2-s3-backup-profile"
-  role = aws_iam_role.ec2_s3_backup_role.name
 }
 
 # 7. (보너스) 실행 완료 후 생성된 버킷 이름을 화면에 출력해 달라는 명령어
