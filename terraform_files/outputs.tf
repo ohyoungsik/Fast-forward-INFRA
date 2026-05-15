ee1d6ef88710b25a6150a51778bb2919ac58959d
wodurl <z94648569@gmail.com>
2026-05-15 16:37:13 +0900

    fix : error fix 3

--- terraform_files/outputs.tf
+++ terraform_files/outputs.tf
@@ -28,3 +28,21 @@ output "ssh_bastion_command" {
 output "ssh_nginx_via_bastion_example" {
   value = "ssh -i ~/.ssh/${var.key_name}.pem -J ubuntu@${aws_instance.bastion_server.public_ip} ubuntu@172.16.20.10"
 }
+
+output "inventory_yml" {
+  value = templatefile("${path.module}/inventory.yml.tpl", {
+    bastion_public_ip = aws_instance.bastion.public_ip
+    bastion_private_ip = aws_instance.bastion.private_ip
+
+    private_server_ips = {
+      for name, instance in aws_instance.private_server :
+      name => instance.private_ip
+    }
+  })
+}
+
+output "ansible_cfg" {
+  value = templatefile("${path.module}/ansible.cfg.tpl", {
+    inventory_path = "../terraform_files/inventory.yml"
+  })
+}
