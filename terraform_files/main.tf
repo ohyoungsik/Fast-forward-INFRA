7b4e93c75f0aab02bf1b82b11d308e0884d2763c
youngsik <dudtlr9308@gmail.com>
2026-05-19 16:16:46 +0900

    fix: 서버 시간 변경 tf -> ansible yml

--- terraform_files/main.tf
+++ terraform_files/main.tf
@@ -129,11 +129,6 @@ resource "aws_instance" "bastion_server" {
     aws_security_group.bastion_sg.id
   ]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              timedatectl set-timezone Asia/Seoul
-              EOF
-
   tags = {
     Name = "bastion-server"
     Role = "public-bastion"
@@ -161,12 +156,6 @@ resource "aws_instance" "private_servers" {
   vpc_security_group_ids = [
     aws_security_group.private_server_sg[each.key].id
   ]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              timedatectl set-timezone Asia/Seoul
-              EOF
-
 
   tags = {
     Name = each.key
